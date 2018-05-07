5881aa854a6a1144aad0aafa1e19db54c3a25cd9
Şeyhmus Aydoğdu <sehmusaydogdu47@gmail.com>
2018-05-07 22:47:10 +0300

    update

--- kredinotu.pl
+++ kredinotu.pl
@@ -1,33 +1,50 @@
 
 yasidogrula(Yas):-Yas>17,write("\nKredi icin yasiniz tutuyor. Devam edebiliriz ...\n").
 yasidogrula(Yas):-Yas<18,write("\nUzgunum. Kredi icin yasiniz uygun degildir.\n"),fail.
+yasidogrula(Yas):-Yas>60,write("\nUzgunum. Kredi icin yasiniz uygun degildir.(50 yaş ve üzeri)\n"),fail.
 
-/*result(Adi,Yas):-write(Adi),write("Yasiniz :  "),write(Yas).*/
 
-ev_adeti(Ev_sayisi) :-write(Ev_sayisi),write("\t evin toplam degerini giriniz : \t"),read(Tutar).
-konutKontrol(Menkul):-Menkul==1,write("\nUzerinizde kayitli bulunan ev sayisi giriniz : \t"),read(Ev_sayisi),ev_adeti(Ev_sayisi).
-konutKontrol(Menkul):-Menkul==2,write("\nKira tutarini giriniz : \t"),read(Tutar),menkulDeger(Tutar).
-konutKontrol(Menkul):-not(Menkul=1),not(Menkul=2),write("\nHatali giris yaptiniz ..."),fail.
-menkulDeger(Tutar):-write("\nGayrimenkul Deger : "),write(Tutar).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==1,Arac==1,Kefil==1,ev_arac_kefil_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==2,Arac==1,Kefil==1,kira_arac_kefil_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==1,Arac==2,Kefil==1,ev_kefil_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==2,Arac==2,Kefil==1,kira_kefil_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==1,Arac==1,Kefil==2,ev_arac_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==2,Arac==1,Kefil==2,kira_arac_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==1,Arac==2,Kefil==2,ev_Doldur(Menkul,Arac,Kefil).
+eksikBilgileriDoldur(Menkul,Arac,Kefil):-Menkul==2,Arac==2,Kefil==2,Kira_Doldur(Menkul,Arac,Kefil).
 
-arac_adeti(Arac_sayisi):-write(Arac_sayisi),
-                         write("\taracin toplam degerini giriniz : \t"),read(AracTutar),
-                         write("Toplam aracin degeri : \t"),write(AracTutar).
+ev_arac_kefil_Doldur(Menkul,Arac,Kefil):- write("\nOturdugunuz evin degerini giriniz : \t"),read(Ev_degeri),
+                                          write("\nUzerinize kayitli bulunan aracin degerini giriniz : \t"),read(Arac_degeri),
+                                          write("\nKefil olacak kisinin maasini giriniz : \t"),read(Kefil_maas).
 
-aracKontrol(Arac):-Arac==1,write("\nUzerinizde kayitli bulunan arac sayisi giriniz : \t"),read(Arac_sayisi),arac_adeti(Arac_sayisi).
-aracKontrol(Arac):-Arac==2,write("").
+kira_arac_kefil_Doldur(Menkul,Arac,Kefil):- write("\nOturdugunuz evin kira degerini giriniz : \t"),read(Kira_degeri),
+                                            write("\nUzerinize kayıtlı bulunan aracin degerini giriniz : \t"),read(Arac_degeri),
+                                            write("\nKefil olacak kisinin maasini giriniz : \t"),read(Kefil_maas).   
 
-kefilKontrol(Kefil):-Kefil == 1, write("\nKefil olacak kisinin maasini giriniz : \t"),read(kefilMaas).
-kefilKontrol(Kefil):-Kefil == 2, write("").
+ev_kefil_Doldur(Menkul,Arac,Kefil):-write("\nOturdugunuz evin degerini giriniz : \t"),read(Ev_degeri),
+                                    write("\nKefil olacak kisinin maasini giriniz : \t"),read(Kefil_maas).
 
-krediKontrolEt(IstenenKredi):-write("\nKullanicinin istemis oldugu kredi miktari : \t"),write(IstenenKredi).
+kira_kefil_Doldur(Menkul,Arac,Kefil):-write("\nOturdugunuz evin kira degerini giriniz : \t"),read(Kira_degeri),
+                                      write("\nKefil olacak kisinin maasini giriniz : \t"),read(Kefil_maas).
+
+ev_arac_Doldur(Menkul,Arac,Kefil):-write("\nOturdugunuz evin degerini giriniz : \t"),read(Ev_degeri),
+                                   write("\nUzerinize kayitli bulunan aracin degerini giriniz : \t"),read(Arac_degeri).
+
+kira_arac_Doldur(Menkul,Arac,Kefil):-write("\nOturdugunuz evin kira degerini giriniz : \t"),read(Kira_degeri),
+                                     write("\nUzerinize kayitli bulunan aracin degerini giriniz : \t"),read(Arac_degeri).
+
+ev_Doldur(Menkul,Arac,Kefil):-write("\nOturdugunuz evin degerini giriniz : \t"),read(Ev_degeri).
+
+kira_Doldur(Menkul,Arac,Kefil):-write("\nOturdugunuz evin kira degerini giriniz : \t"),read(Kira_degeri).
 
 main:-
 write("\nAdinizi giriniz : \t"),read(Adi),write(Adi),write("\t Merhabalar \n"),
 write("\nYasinizi giriniz  : \t"),read(Yas),yasidogrula(Yas),
-write("\nMesleginizi giriniz  : \t"),read(Meslek),
+write("\nMesleginizi giriniz  (Ogrenci => 1 , Calisan => 2 , Issiz => 3): \t"),read(Meslek),
 write("\nMaasinizi giriniz : \t"),read(Maas),
-write("\nBulundugunuz konut (Ev => 1 , Kira => 2) : \t"),read(Menkul),konutKontrol(Menkul),
-write("\nUzerinizde kayitli bulunan arac var mi? (Evet => 1 , Hayir => 2) : \t"),read(Arac),aracKontrol(Arac),
-write("\nKefil olacak kisi var mi? (Evet => 1 , Hayir => 2) : \t"),read(Kefil),kefilKontrol(Kefil),
-write("\nIstediginiz kredi tutarini giriniz : \t"),read(IstenenKredi),krediKontrolEt(IstenenKredi).
+write("\nOturdugunuz konut turu (Ev => 1 , Kira => 2) : \t"),read(Menkul),
+write("\nUzerinizde kayitli bulunan arac var mi? (Evet => 1 , Hayir => 2) : \t"),read(Arac),
+write("\nKefil olacak kisi var mi? (Evet => 1 , Hayir => 2) : \t"),read(Kefil),
+write("\nIstediginiz kredi tutarini giriniz : \t"),read(IstenenKredi),
+write("\n\nBilgiler hesaplanıyor\n"),
+eksikBilgileriDoldur(Menkul,Arac,Kefil).
